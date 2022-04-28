e9c04a0bd78dbeb4eb29252da6280a9aa402a883
Glitch (glitch-hello-node) <none>
2022-04-28 17:22:01 +0000

    ⚓️👭 Checkpoint ./server.js:37345865/312 ./src/pages/bgscene.hbs:37345865/92 ./.gitignore:37345865/24

--- server.js
+++ server.js
@@ -88,14 +88,14 @@ fastify.get("/bgscene", function(request, reply) {
 
 fastify.get("/nigiyaka", function(request, reply) {
     db.serialize(() => {
-//    console.log(sql.geticon);
     var pre = db.prepare(sql.geticon);
-    pre.bind(request.body.code,function(){
+    pre.bind(request.query['code'],function(){
       pre.get((err, rows) => {
       //    console.log(JSON.stringify(rows));
         reply
           .code(200)
           .header("Content-Type","application/json; charset=utf-8")
+//          .send({"icon":"fa-ghost","code":"20220430010101"});
           .send(JSON.stringify(rows));
       });
     });
